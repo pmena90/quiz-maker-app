6fbeb42a524fbdd842e4736b90bd316dd8caf2c2
Pavel Mena <pavel.mena1990@gmail.com>
2023-08-29 14:32:19 +0200

    Updates

---
+++ static/js/811.7e576fe3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_app_context=self.webpackChunkreact_app_context||[]).push([[811],{811:function(e,n,s){s.r(n);var t=s(791),c=s(541),i=s(971),r=s(640),a=s(184);n.default=function(){var e=(0,t.useContext)(c.C),n=e.questions;e.handleAnswerSelection,n.find((function(e){return""===e.selectedAnswer}));return(0,a.jsxs)("div",{className:"container",children:[(0,a.jsx)("div",{className:"row",children:(0,a.jsx)(r.Z,{title:"RESULTS"})}),(0,a.jsx)("div",{className:"row",children:(0,a.jsx)(i.Z,{isFinished:!0})})]})}}}]);
+//# sourceMappingURL=811.7e576fe3.chunk.js.map
